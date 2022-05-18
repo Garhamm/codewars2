a3714174074ae9e654435ec64c6645b167f18b97
Garrett Hamman <50977644+Garhamm@users.noreply.github.com>
2022-05-17 19:29:01 -0500

    first upload

--- js/solution001.js
+++ js/solution001.js
@@ -3,10 +3,10 @@
 function between(a, b) {
     let newArr = []
     for (let i = a; i<=b; i++) {
-         newArr += [i]
-         console.log(newArr)
+         newArr.push(i)
     }
     return newArr
   }
 
  console.log(between(1,4))
+ console.log(between(2,4300))
