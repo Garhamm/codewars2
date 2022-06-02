4345276d39e9e56690bb0dd45a46ea28f38aa931
Garrett Hamman <50977644+Garhamm@users.noreply.github.com>
2022-06-01 21:04:10 -0500

    another solution

--- js/solution024.js
+++ js/solution024.js
@@ -1,1 +1,17 @@
-//sdsfsfd
+
+// 8 kyu
+// Reversed sequence
+// 35010892% of 5,68820,518 of 54,809jamad1 Issue Reported
+
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+const reverseSeq = n => {
+    let arr = []
+    for (let i = 1; i <= n; i++){
+        arr.push(i)
+    }return arr.reverse()
+  };
+
+  console.log(reverseSeq(12))
